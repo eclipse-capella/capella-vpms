fed8edb912bb7b59f2c0e384efc6e2415786e09e
Felix Dorner <felix.dorner@gmail.com>
2020-02-05 13:51:53 +0100

    [559497] Remove copied code
    Superclass is no longer anonymous, copied code can be removed.
    
    Signed-off-by: Felix Dorner <felix.dorner@gmail.com>

--- plugins/org.polarsys.capella.vp.ms.edit/src/org/polarsys/capella/vp/ms/provider/CSConfigurationItemPropertyDescriptor.java
+++ plugins/org.polarsys.capella.vp.ms.edit/src/org/polarsys/capella/vp/ms/provider/CSConfigurationItemPropertyDescriptor.java
@@ -15,13 +15,11 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
-import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.util.ResourceLocator;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.polarsys.capella.common.data.modellingcore.ModelElement;
-import org.polarsys.capella.common.mdsofa.common.helper.ExtensionPointHelper;
-import org.polarsys.capella.common.model.IDelegatedListener;
+import org.polarsys.capella.common.data.modellingcore.provider.ModelElementItemPropertyDescriptor;
 import org.polarsys.capella.core.data.cs.Component;
 import org.polarsys.capella.core.data.fa.AbstractFunction;
 import org.polarsys.capella.core.data.fa.FunctionalExchange;
@@ -29,44 +27,14 @@ import org.polarsys.capella.core.data.information.Port;
 import org.polarsys.capella.core.data.interaction.Scenario;
 import org.polarsys.capella.vp.ms.CSConfiguration;
 import org.polarsys.capella.vp.ms.MsPackage;
-import org.polarsys.kitalpha.emde.model.edit.provider.ExtensionItemPropertyDescriptor;
 
-public class CSConfigurationItemPropertyDescriptor extends ExtensionItemPropertyDescriptor {
+public class CSConfigurationItemPropertyDescriptor extends ModelElementItemPropertyDescriptor {
 
   public CSConfigurationItemPropertyDescriptor(AdapterFactory adapterFactory, ResourceLocator resourceLocator,
       String displayName, String description, EStructuralFeature feature, boolean isSettable, boolean multiLine,
       boolean sortChoices, Object staticImage, String category, String[] filterFlags) {
     super(adapterFactory, resourceLocator, displayName, description, feature, isSettable, multiLine, sortChoices,
         staticImage, category, filterFlags);
-  }
-
-  // FIXME: canSetProperty is copied from ModelElementItemProvider.createPropertyDescriptor
-  // Depends on https://bugs.polarsys.org/show_bug.cgi?id=739
-  /**
-   * @see org.polarsys.capella.common.mdsofa.emf.edit.provider.DslfactoryItemPropertyDescriptor#canSetProperty(java.lang.Object)
-   */
-  @Override
-  public boolean canSetProperty(Object object_p) {
-    boolean result = true;
-    for (IConfigurationElement configurationElement : ExtensionPointHelper
-        .getConfigurationElements("org.polarsys.capella.common.model", "DelegatedListener")) { //$NON-NLS-1$ //$NON-NLS-2$
-      IDelegatedListener contributor = (IDelegatedListener) ExtensionPointHelper.createInstance(configurationElement,
-          ExtensionPointHelper.ATT_CLASS);
-      if (null != contributor) {
-        result &= contributor.canSetProperty(object_p);
-      }
-    }
-    if (result) {
-      result &= super.canSetProperty(object_p);
-    }
-    return result;
-    // TODO mettre en place un point d'extension qui sera implemente par le plugin collab capella.
-    // En effet, ce dernier branchera le canSetProperty sur le lock cdo.
-    // IPropertyEdition propertyEdition = null;
-    // if (null == propertyEdition) {
-    // return super.canSetProperty(object_p);
-    // }
-    // return propertyEdition.canSetProperty(object_p);
   }
 
   @Override
@@ -88,7 +56,9 @@ public class CSConfigurationItemPropertyDescriptor extends ExtensionItemProperty
       return result;
     }
 
-    return ((CSConfiguration) object).getScope((Class<? extends ModelElement>) feature.getEType().getInstanceClass());
+    @SuppressWarnings("unchecked")
+    Class<? extends ModelElement> type = (Class<? extends ModelElement>) feature.getEType().getInstanceClass();
+    return ((CSConfiguration) object).getScope(type);
   }
 
 }
--- plugins/org.polarsys.capella.vp.ms.edit/src/org/polarsys/capella/vp/ms/provider/ChildConfigurationsPropertyDescriptor.java
+++ plugins/org.polarsys.capella.vp.ms.edit/src/org/polarsys/capella/vp/ms/provider/ChildConfigurationsPropertyDescriptor.java
@@ -21,59 +21,27 @@ import java.util.Deque;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.util.ResourceLocator;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
-import org.polarsys.capella.common.mdsofa.common.helper.ExtensionPointHelper;
-import org.polarsys.capella.common.model.IDelegatedListener;
+import org.polarsys.capella.common.data.modellingcore.provider.ModelElementItemPropertyDescriptor;
 import org.polarsys.capella.core.data.cs.Component;
 import org.polarsys.capella.core.data.la.LogicalComponent;
 import org.polarsys.capella.core.data.pa.PhysicalComponent;
 import org.polarsys.capella.vp.ms.CSConfiguration;
 import org.polarsys.kitalpha.emde.model.ExtensibleElement;
-import org.polarsys.kitalpha.emde.model.edit.provider.ExtensionItemPropertyDescriptor;
 
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 
-public class ChildConfigurationsPropertyDescriptor extends ExtensionItemPropertyDescriptor {
+public class ChildConfigurationsPropertyDescriptor extends ModelElementItemPropertyDescriptor {
 
   public ChildConfigurationsPropertyDescriptor(AdapterFactory adapterFactory, ResourceLocator resourceLocator,
       String displayName, String description, EStructuralFeature feature, boolean isSettable, boolean multiLine,
       boolean sortChoices, Object staticImage, String category, String[] filterFlags) {
     super(adapterFactory, resourceLocator, displayName, description, feature, isSettable, multiLine, sortChoices,
         staticImage, category, filterFlags);
-  }
-
-  // FIXME: canSetProperty is copied from ModelElementItemProvider.createPropertyDescriptor
-  // Depends on https://bugs.polarsys.org/show_bug.cgi?id=739
-  /**
-   * @see org.polarsys.capella.common.mdsofa.emf.edit.provider.DslfactoryItemPropertyDescriptor#canSetProperty(java.lang.Object)
-   */
-  @Override
-  public boolean canSetProperty(Object object_p) {
-    boolean result = true;
-    for (IConfigurationElement configurationElement : ExtensionPointHelper
-        .getConfigurationElements("org.polarsys.capella.common.model", "DelegatedListener")) { //$NON-NLS-1$ //$NON-NLS-2$
-      IDelegatedListener contributor = (IDelegatedListener) ExtensionPointHelper.createInstance(configurationElement,
-          ExtensionPointHelper.ATT_CLASS);
-      if (null != contributor) {
-        result &= contributor.canSetProperty(object_p);
-      }
-    }
-    if (result) {
-      result &= super.canSetProperty(object_p);
-    }
-    return result;
-    // TODO mettre en place un point d'extension qui sera implemente par le plugin collab capella.
-    // En effet, ce dernier branchera le canSetProperty sur le lock cdo.
-    // IPropertyEdition propertyEdition = null;
-    // if (null == propertyEdition) {
-    // return super.canSetProperty(object_p);
-    // }
-    // return propertyEdition.canSetProperty(object_p);
   }
 
   private Collection<CSConfiguration> getOwnedConfigurations(ExtensibleElement e,
