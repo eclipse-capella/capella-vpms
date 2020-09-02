78dc32c7a12678663030732fd4c21341a0fc72f3
Felix Dorner <felix.dorner@gmail.com>
2020-09-02 14:07:47 +0200

    [566548] Better Change Expression action enablement check
    Change Expression action should only be enabled on lines that have State
    Machines.
    
    Signed-off-by: Felix Dorner <felix.dorner@gmail.com>

--- plugins/org.polarsys.capella.vp.ms.ui/src/org/polarsys/capella/vp/ms/ui/DiagramPropertyTester.java
+++ plugins/org.polarsys.capella.vp.ms.ui/src/org/polarsys/capella/vp/ms/ui/DiagramPropertyTester.java
@@ -3,13 +3,24 @@ package org.polarsys.capella.vp.ms.ui;
 import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.sirius.diagram.DDiagram;
 import org.eclipse.sirius.diagram.ui.tools.api.editor.DDiagramEditor;
+import org.eclipse.sirius.table.metamodel.table.DCell;
+import org.eclipse.sirius.table.metamodel.table.DLine;
 import org.eclipse.sirius.table.metamodel.table.DTable;
 import org.eclipse.sirius.table.ui.tools.api.editor.DTableEditor;
 
 public class DiagramPropertyTester extends PropertyTester {
-  
+
   @Override
   public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
+    if ("lineId".equals(property)) {
+      if (receiver instanceof DCell) {
+        receiver = ((DCell)receiver).getLine();
+      }
+      if (receiver instanceof DLine) {
+        return expectedValue.equals(((DLine) receiver).getMapping().getName());
+      }
+      return false;
+    }
     if ("description".equals(property)) {
       if (receiver instanceof DTableEditor) {
         final DTableEditor editor = (DTableEditor) receiver;
@@ -25,7 +36,5 @@ public class DiagramPropertyTester extends PropertyTester {
     }
     throw new IllegalArgumentException("Unknown Property: " + property);
   }
-  
-
 
 }
