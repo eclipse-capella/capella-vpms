47c145fd299abeadf04ba50061b50eb4082d121a
Felix Dorner <felix.dorner@gmail.com>
2020-12-09 10:47:59 +0100

    Add simple transition test
    Signed-off-by: Felix Dorner <felix.dorner@gmail.com>

---
+++ plugins/org.polarsys.capella.vp.ms.tests/src/org/polarsys/capella/vp/ms/transition/test/MsTransitionTest1.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2020 THALES GLOBAL SERVICES.
+ *  
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.vp.ms.transition.test;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.polarsys.capella.common.data.modellingcore.ModelElement;
+import org.polarsys.capella.core.data.la.LogicalComponent;
+import org.polarsys.capella.test.transition.ju.TopDownTransitionTestCase;
+import org.polarsys.capella.vp.ms.CSConfiguration;
+
+public class MsTransitionTest1 extends TopDownTransitionTestCase {
+
+  @Override
+  public List<String> getRequiredTestModels() {
+    return Collections.singletonList("MsTransitionTest");
+  }
+
+  @Override
+  public void performTest() throws Exception {
+    // TODO Auto-generated method stub
+    
+    LogicalComponent lc = getObject("8d465cd0-5a81-4b15-85f6-9315fe026a90");
+    
+    CSConfiguration c = getObject("a234fcac-821c-46dc-a6f1-1a5a73a4bf18");
+    Collection<ModelElement> excluded = c.getExcluded();
+    Collection<ModelElement> included = c.getIncluded();
+
+    assertFalse(excluded.isEmpty());
+    assertFalse(included.isEmpty());
+    
+    performLCtoPCTransition(Collections.singleton(lc));
+
+    CSConfiguration cstar = mustBeMonoTransitioned(c);
+    assertEquals(cstar.getExcluded().size(), excluded.size());
+    assertEquals(cstar.getIncluded().size(), included.size());
+    for (ModelElement e : cstar.getExcluded()) {
+      for (EObject traced : getAllocatedElements(e)) {
+        assertTrue(c.getExcluded().contains(traced));
+      }
+    }
+    for (ModelElement e : cstar.getIncluded()) {
+      for (EObject traced : getAllocatedElements(e)) {
+        assertTrue(c.getIncluded().contains(traced));
+      }
+    }
+    
+    assertEquals(c.getName(), cstar.getName());
+    assertEquals(c.getKind(), cstar.getKind());
+
+  }
+
+}
